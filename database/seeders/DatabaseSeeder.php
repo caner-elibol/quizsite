f4a6f5c9119c321625d09c545f6c460070b9a41a
elibol97 <caner.elibol@hotmail.com>
2021-05-12 02:41:49 +0300

    quizsite v1.3 (QuizSeeders migration)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,14 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::insert([
-            'name' => 'Caner ELİBOL',
-            'email' => 'elibol97@gmail.com',
-            'type'=>'admin',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$pVqtY8fN5uXv2Sj28l/VOuX7s7.a31Yb79NqJqAYT0GVEDINjn2mC', // password
-            'remember_token' => Str::random(10),
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::class,            
         ]);
-        \App\Models\User::factory(5)->create();
     }
 }
