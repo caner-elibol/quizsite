8295ee089264b0011f7a88b672ecb6ea74566981
elibol97 <caner.elibol@hotmail.com>
2021-05-12 03:34:32 +0300

    quizsite v1.4(FA ve Quizlerin listelenmesi)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\QuizController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/panel', function () {
 })->name('dashboard'); 
 
 Route::group(['middleware'=>['auth','isAdmin'],'prefix'=>'admin'], function (){
-Route::get('deneme', function () {
-    return "prefix Test";
-});
+    Route::resource('quizzes',QuizController::class);
 });
